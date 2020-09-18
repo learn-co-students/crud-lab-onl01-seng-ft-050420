389b45cb09d6b926d896e579924d233f2e4e5d81
leobr <leobrionesherrera@gmail.com>
2020-09-18 17:22:54 -0500

    Done.

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import ReviewInput from '../components/reviews/ReviewInput';
 import Reviews from '../components/reviews/Reviews';
+import { connect } from 'react-redux'
 
 class ReviewsContainer extends Component {
 
   render() {
+    const reviews = this.props.reviews.filter(el => el.restaurantId === this.props.restaurant.id)
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurantId={this.props.restaurant.id} />
+        <Reviews reviews={reviews} deleteReview={this.props.deleteReview} />
       </div>
     )
   }
 }
 
-export default ReviewsContainer;
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addReview: review => dispatch({ type: "ADD_REVIEW", review: review }),
+    deleteReview: review => dispatch({ type: "DELETE_REVIEW", review: review }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer);
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,21 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state={restaurants: [], reviews: []}, action) {
+    switch(action.type) {
+        case "ADD_RESTAURANT":
+            const restaurant = {text: action.text, id: cuid()};
+            return {...state, restaurants: [...state.restaurants, restaurant]}
+        case "DELETE_RESTAURANT": 
+            const restaurants = state.restaurants.filter(el => el.id !== action.id);
+            return {...state, restaurants: restaurants};
+        case "ADD_REVIEW":
+            return {...state, reviews: [...state.reviews, action.review]};
+        case "DELETE_REVIEW":
+            
+            const reviews = state.reviews.filter(el => el.text !== action.review.text);
+           
+            return {...state, reviews }
+        default:
+            return state;
+    }
 }
