2c7a0d6e335a22886b983351a1f92806fa3bfe49
claudiahalip <claudia4ro@yahoo.com>
2020-09-23 11:23:21 -0500

    Done.

---
+++ src/components/actions.js
@@ -0,0 +1,15 @@
+export function addRestaurant(name){
+    return {type: 'ADD_RESTAURANT', payload: name}
+}
+
+export function deleteRestaurant(id){
+    return {type: 'DELETE_RESTAURANT', payload: id}
+}
+
+export function addReview(review) {
+    return { type: 'ADD_REVIEW', payload: review}
+}
+
+export function deleteReview(id){
+    return {type: 'DELETE_REVIEW', payload: id}
+}
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,25 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state ={
+    restaurants: [],
+    reviews: []
+}, action) {
+    switch(action.type){
+        case 'ADD_RESTAURANT':
+            const restaurant = {
+                id: cuid(),
+                text: action.payload
+            }
+            return {...state, restaurants: [...state.restaurants, restaurant]}
+        case 'DELETE_RESTAURANT':
+            return {...state, restaurants: state.restaurants.filter(rest => rest.id !== action.payload)}
+        case 'ADD_REVIEW':
+            const review = { id: cuid(), text: action.payload.text, restaurantId: action.payload.restaurantId }
+            return{...state, reviews: [...state.reviews, review]}
+        case 'DELETE_REVIEW':
+            return {...state, reviews: state.reviews.filter(review => review.id !== action.payload)}
+        default:
+            return state;       
+    }
 
 }
